5caa3cafea2dd2b9fee972dad83a4188dd325d28
pedrogaytan <95668093+pedrogaytan@users.noreply.github.com>
2022-04-26 19:32:03 -0500

    updates

---
+++ 19.1-activities/01_Hello_World_Again/unsolved/js/index.js
@@ -0,0 +1,8 @@
+// Create a variable below
+//var helloWorld = 'Hello World!';*
+
+function myFunction() {
+    alert("Hello! I am an alert box!");
+  }
+// Console.log() your variable here.
+console.log(helloWorld);
